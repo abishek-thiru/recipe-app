ec121263a3445bf16e4fb5538b3dde5034f8977a
Abishek Thirumoorthi <92705641+abishek-thiru@users.noreply.github.com>
2024-10-12 05:30:33 +0530

    Create RecipeCard.js
    Components added.

---
+++ components/RecipeCard.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function RecipeCard({ recipe }) {
+  return (
+    <div className="bg-white border border-gray-200 rounded-[25px] p-8 shadow-md hover:shadow-lg transition-shadow duration-200 m-4 h-[200px] w-[300px] md:w-[350px] lg:w-[300px]">
+      <h2 className="text-[#229799] text-xl font-semibold mb-3">
+        {recipe.title}
+      </h2>
+      <p className="text-[#424242] mb-4">{recipe.description}</p>
+      <Link
+        className="bg-[#48CFCB] hover:bg-[#48CFCB] text-white font-semibold py-2 px-6 rounded"
+        href={`/recipes/${recipe.id}`}
+      >
+        View Recipe
+      </Link>
+    </div>
+  );
